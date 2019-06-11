9f469f7c861e5a6b873c88e0ff692d52e0d855fe
Rachael Keller <rachaeltara.keller@gmail.com>
2019-06-11 08:42:58 -0500

    added loop over 10-day period to pull data

--- DataGrabber.py
+++ DataGrabber.py
@@ -27,63 +27,56 @@ one_sec = datetime.timedelta(seconds=1)
 # utc offset (cdt)
 utc_offset = datetime.timedelta(hours=5);
 
-begin = datetime.datetime(2019, 6, 8, 0, 0, 0)
-end   = datetime.datetime(2019, 6, 9, 0, 0);
-starttime = '{0:%Y-%m-%d+%H:%M:%S}'.format(end-utc_offset - one_day)
-stopptime = '{0:%Y-%m-%d+%H:%M:%S}'.format(end-utc_offset)
+end   = datetime.datetime(2019, 6, 11, 0, 0);
 
-# logger_get ACL command documentation: https://www-bd.fnal.gov/issues/wiki/ACLCommandLogger_get
-URL = "http://www-ad.fnal.gov/cgi-bin/acl.pl?acl=logger_get/date_format='utc_seconds'/ignore_db_format/start=\""+starttime+"\"/end=\""+stopptime+"\"/node="
+for _ in xrange(10)
+    starttime = '{0:%Y-%m-%d+%H:%M:%S}'.format(end-utc_offset - one_day)
+    stopptime = '{0:%Y-%m-%d+%H:%M:%S}'.format(end-utc_offset)
+    end = end - one_day;
 
-D43DataLoggerNode = 'MLrn'
-URL = URL + D43DataLoggerNode + '+'
-deviceNames = ['B:VIMIN', 'B_VIMIN', 'B:VIMAX', 'B_VIMAX', 'B:IMINER', 'B:NGMPS', 'B:VINHBT', 'B:GMPSFF', 'B:GMPSBT',
-               'B:IMINST', 'B:IPHSTC', 'B:IMINXG', 'B:IMINXO', 'B:IMAXXG', 'B:IMAXXO', 'B_VINHBT', 'B_GMPSFF', 'B_GMPSBT',
-               'B_IMINST', 'B_IPHSTC', 'B_IMINXG', 'B_IMINXO', 'B_IMAXXG', 'B_IMAXXO',
-               'B:ACMNPG', 'B:ACMNIG', 'B:ACMXPG', 'B:ACMXIG', 'B:DCPG' , 'B:DCIG', 'B:VIPHAS',
-               'B_ACMNPG', 'B_ACMNIG', 'B_ACMXPG', 'B_ACMXIG', 'B_DCPG' , 'B_DCIG', 'B_VIPHAS',
-               'B:PS1VGP', 'B:PS1VGM', 'B:GMPS1V', 'B:PS2VGP', 'B:PS2VGM', 'B:GMPS2V', 'B:PS3VGP', 'B:PS3VGM', 'B:GMPS3V', 'B:PS4VGP', 'B:PS4VGM', 'B:GMPS4V']
+    # logger_get ACL command documentation: https://www-bd.fnal.gov/issues/wiki/ACLCommandLogger_get
+    URL = "http://www-ad.fnal.gov/cgi-bin/acl.pl?acl=logger_get/date_format='utc_seconds'/ignore_db_format/start=\""+starttime+"\"/end=\""+stopptime+"\"/node="
 
+    D43DataLoggerNode = 'MLrn'
+    URL = URL + D43DataLoggerNode + '+'
+    deviceNames = ['B:VIMIN', 'B_VIMIN', 'B:VIMAX', 'B_VIMAX', 'B:IMINER', 'B:NGMPS', 'B:VINHBT', 'B:GMPSFF', 'B:GMPSBT',
+                   'B:IMINST', 'B:IPHSTC', 'B:IMINXG', 'B:IMINXO', 'B:IMAXXG', 'B:IMAXXO', 'B_VINHBT', 'B_GMPSFF', 'B_GMPSBT',
+                   'B_IMINST', 'B_IPHSTC', 'B_IMINXG', 'B_IMINXO', 'B_IMAXXG', 'B_IMAXXO',
+                   'B:ACMNPG', 'B:ACMNIG', 'B:ACMXPG', 'B:ACMXIG', 'B:DCPG' , 'B:DCIG', 'B:VIPHAS',
+                   'B_ACMNPG', 'B_ACMNIG', 'B_ACMXPG', 'B_ACMXIG', 'B_DCPG' , 'B_DCIG', 'B_VIPHAS',
+                   'B:PS1VGP', 'B:PS1VGM', 'B:GMPS1V', 'B:PS2VGP', 'B:PS2VGM', 'B:GMPS2V', 'B:PS3VGP', 'B:PS3VGM', 'B:GMPS3V', 'B:PS4VGP', 'B:PS4VGM', 'B:GMPS4V']
+    tempfilename = 'temp_file.txt'
+    timestamps = np.zeros(shape=(1,1))
 
-tempfilename = 'temp_file.txt'
-timestamps = np.zeros(shape=(1,1))
+    dfdict = {} #Need a place to keep each dataframe
+    for deviceName in deviceNames:
+        tempfilename = 'tempfile'+deviceName+'.txt'
+        tempURL = URL + deviceName
+        if debug: print (tempURL)
 
-dfdict = {} #Need a place to keep each dataframe
-for deviceName in deviceNames:
-    tempfilename = 'tempfile'+deviceName+'.txt'
-    tempURL = URL + deviceName
-    if debug: print (tempURL)
+        # Download device data to local ASCII file
+        with open(tempfilename, "wb") as file:
+            # Column headers
+            headers = 'utc_seconds'+deviceName+' \t '+deviceName+'\n'
+            # Write headers encoded
+            file.write(headers.encode('utf-8'))
+            # Get request
+            response = requests.get(tempURL)
+            if str(response.content).count('logger_get') > 0:
+                print (response.content) #Should go to a log file. 
+                exit()
+            # Write data to file
+            file.write(response.content)
+        # Dump the file into a pandas DataFrame 
+        time_str = 'time_'
+        columns = (time_str+deviceName, deviceName) # Will get these set up higher.
+        dfdict[deviceName] = pd.read_csv(tempfilename, delim_whitespace=True, names=columns, skiprows=1)
+        timestamps_thisdevice = pd.to_datetime(dfdict[deviceName][time_str+deviceName].values, unit = 's');
+        dfdict[deviceName][time_str + deviceName] = timestamps_thisdevice; # convert to conventional time
+        if debug: print (dfdict[deviceName])
 
-    # Download device data to local ASCII file
-    with open(tempfilename, "wb") as file:
-        # Column headers
-        headers = 'utc_seconds'+deviceName+' \t '+deviceName+'\n'
-        # Write headers encoded
-        file.write(headers.encode('utf-8'))
-        # Get request
-        response = requests.get(tempURL)
-        if str(response.content).count('logger_get') > 0:
-            print (response.content) #Should go to a log file. 
-            exit()
-        # Write data to file
-        file.write(response.content)
-    # Dump the file into a pandas DataFrame 
-    time_str = 'time_'
-    columns = (time_str+deviceName, deviceName) # Will get these set up higher.
-    dfdict[deviceName] = pd.read_csv(tempfilename, delim_whitespace=True, names=columns, skiprows=1)
-    timestamps_thisdevice = pd.to_datetime(dfdict[deviceName][time_str+deviceName].values, unit = 's');
-    dfdict[deviceName][time_str + deviceName] = timestamps_thisdevice; # convert to conventional time
-    if debug: print (dfdict[deviceName])
-
-if debug: print (dfdict.values())
-df = pd.concat(dfdict.values(), axis=1)
-h5key = 'x' #str(time.time())
-#Fun with hdf5
-#df.to_hdf('moardata.h5', key=h5key, mode='w')
-st1 = starttime.replace('+','-')
-st2 = stopptime.replace('+','-')
-st1 = st1.replace(':','-')
-st2 = st2.replace(':','-')
-st1 = st1.replace('-','')
-st2 = st2.replace('-','')
-df.to_csv(st1+'-'+st2+'.csv')
+    if debug: print (dfdict.values())
+    df = pd.concat(dfdict.values(), axis=1)
+    st1 = starttime.replace('+','').replace(':','').replace('-','')
+    st2 = stopptime.replace('+','').replace(':','').replace('-','')
+    df.to_csv(st1+'-'+st2+'.csv')
